b2af26268e85ba13a5c0f002413b069f6c11a9fe
zmyzy <49664384+zmyzy@users.noreply.github.com>
2019-04-27 08:41:53 +0800

    Update ClassesSTART_PLace.sql
    添加备注

--- ClassesSTART_PLace.sql
+++ ClassesSTART_PLace.sql
@@ -1,9 +1,9 @@
 SET
 @MAP := 'PUT MAP ID HERE',
-@ZONE := 'PUT ZONE ID HERE',
+@ZONE := 'PUT ZONE ID HERE',/地区/
 @X := 'PUT COORDINATES X HERE',
 @Y := 'PUT COORDINATES Y HERE',
 @Z := 'PUT COORDINATES Z HERE',
-@O := 'PUT ORIENTATION HERE';
+@O := 'PUT ORIENTATION HERE';/修改方向/
 
 UPDATE playercreateinfo SET map=@MAP, zone=@ZONE, position_X=@X, position_Y=@Y, position_Z=@Z, orientation=@O WHERE race IN(1, 2, 3, 4, 5, 6, 7, 8, 10, 11);
